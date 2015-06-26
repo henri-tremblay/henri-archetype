026db77e5cca9d379001bf7a97d13dbb4f380acc
Henri Tremblay <henri.tremblay@gmail.com>
2015-06-26 09:21:59 -0400

    Nice archetype for Java 8

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
